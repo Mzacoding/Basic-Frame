9839027a91d0103c4611ab9d63b7f77ba919b02e
Nyiko Mkansi <134933325+Mzacoding@users.noreply.github.com>
2024-07-06 20:39:09 +0200

    Add files via upload

---
+++ ao/tut4life/MyFrame/MyFrame.java
@@ -0,0 +1,19 @@
+
+package ao.tut4life.MyFrame;
+
+import javax.swing.JFrame;
+
+
+public class MyFrame extends JFrame {
+
+    public MyFrame()  {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocation(500,200);
+        this.setSize(500,500);
+        this.setTitle("BasicFrame");
+        this.setLayout(null);
+        this.setVisible(true);
+    }
+    
+    
+}
---
+++ myframeapp/java/MyFrameAppJava.java
@@ -0,0 +1,16 @@
+
+package myframeapp.java;
+
+import ao.tut4life.MyFrame.MyFrame;
+
+
+public class MyFrameAppJava {
+
+  
+    public static void main(String[] args) {
+    
+        
+        new MyFrame();
+    }
+    
+}
